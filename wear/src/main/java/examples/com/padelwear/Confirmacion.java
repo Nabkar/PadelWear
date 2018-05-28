27708bc986c8ab9cb6779300aad87604ede7cc7e
Julio Albelda <juliet.xxl@gmail.com>
2018-05-28 19:55:17 +0200

    Initial commit

---
+++ wear/src/main/java/examples/com/padelwear/Confirmacion.java
@@ -0,0 +1,13 @@
+package examples.com.padelwear;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import examples.com.padelwear.R;
+
+public class Confirmacion  extends Activity {
+    @Override protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.confirmacion);
+    }
+}
---
+++ wear/src/main/java/examples/com/padelwear/MainActivity.java
@@ -0,0 +1,52 @@
+package examples.com.padelwear;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.wear.widget.WearableLinearLayoutManager;
+import android.support.wear.widget.WearableRecyclerView;
+import android.support.wearable.activity.WearableActivity;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends WearableActivity {
+    // Elementos a mostrar en la lista
+    String[] elementos = {"Partida", "Terminar partida", "Historial",
+            "Notificación", "Pasos", "Pulsaciones", "Terminar partida" };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WearableRecyclerView lista = (WearableRecyclerView)
+                findViewById(R.id.lista);
+        lista.setEdgeItemsCenteringEnabled(true);
+        lista.setLayoutManager(new WearableLinearLayoutManager(this,
+                new CustomLayoutCallback()));
+        Adaptador adaptador = new Adaptador(this, elementos);
+        adaptador.setOnItemClickListener(
+            new View.OnClickListener() {
+                 @Override
+                 public void onClick(View v) {
+                     Integer tag = (Integer) v.getTag();
+                     //Toast.makeText(MainActivity.this, "Elegida opción:" + tag,Toast.LENGTH_SHORT).show();
+                     switch (tag) {
+                         case 1:
+                             startActivity(new Intent(MainActivity.this, Confirmacion.class));
+                             break;
+                         case 2:
+                             startActivity(new Intent(MainActivity.this, Historial.class));
+                             break;
+                         case 4:
+                             startActivity(new Intent(MainActivity.this, Pasos.class));
+                             break;
+
+                     }
+                 }
+            }
+        );
+        lista.setAdapter(adaptador);
+        lista.setCircularScrollingGestureEnabled(true);
+        lista.setScrollDegreesPerScreen(180);
+        lista.setBezelFraction(0.5f);
+    }
+}
