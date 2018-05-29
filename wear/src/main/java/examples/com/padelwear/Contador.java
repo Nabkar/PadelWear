4e6f2812396a0ddf33dafef87f3d0a6deb4bc9df
Unknown <juliet.xxl@gmail.com>
2018-05-29 22:00:06 +0200

    Diap 49

---
+++ wear/src/main/java/examples/com/padelwear/Contador.java
@@ -0,0 +1,181 @@
+package examples.com.padelwear;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.os.Vibrator;
+import android.support.wearable.activity.WearableActivity;
+import android.support.wearable.view.DismissOverlayView;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.comun.DireccionesGestureDetector;
+import com.example.comun.Partida;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class Contador extends WearableActivity {
+	private Partida partida;
+	private TextView misPuntos, misJuegos, misSets,
+			susPuntos, susJuegos, susSets, hora;
+	private Vibrator vibrador;
+	private long[] vibrEntrada = {0l, 500};
+	private long[] vibrDeshacer = {0l, 500, 500, 500};
+
+	private DismissOverlayView dismissOverlay;
+
+	private Typeface fuenteNormal = Typeface.create("sans-serif",
+			Typeface.NORMAL);
+	private Typeface fuenteFina = Typeface.create("sans-serif-thin",
+			Typeface.NORMAL);
+
+
+	@SuppressLint("ClickableViewAccessibility")
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.contador);
+		setAmbientEnabled();
+
+		partida = new Partida();
+		vibrador = (Vibrator) this.getSystemService(Context
+				.VIBRATOR_SERVICE);
+		misPuntos = (TextView) findViewById(R.id.misPuntos);
+		susPuntos = (TextView) findViewById(R.id.susPuntos);
+		misJuegos = (TextView) findViewById(R.id.misJuegos);
+		susJuegos = (TextView) findViewById(R.id.susJuegos);
+		misSets = (TextView) findViewById(R.id.misSets);
+		susSets = (TextView) findViewById(R.id.susSets);
+		hora = findViewById(R.id.hora);
+		actualizaNumeros();
+		View fondo = findViewById(R.id.fondo);
+
+		//getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+		fondo.setOnTouchListener(new View.OnTouchListener() {
+			GestureDetector detector = new DireccionesGestureDetector(
+					Contador.this,
+					new DireccionesGestureDetector
+							.SimpleOnDireccionesGestureListener() {
+						@Override public boolean onArriba(MotionEvent e1,
+														  MotionEvent e2, float distX, float distY) {
+							partida.rehacerPunto();
+							vibrador.vibrate(vibrDeshacer, -1);
+							actualizaNumeros();
+							return true;
+						}
+						@Override public boolean onAbajo(MotionEvent e1,
+														 MotionEvent e2, float distX, float distY) {
+							partida.deshacerPunto();
+							vibrador.vibrate(vibrDeshacer, -1);
+							actualizaNumeros();
+							return true;
+						}
+						@Override
+						public void onLongPress(MotionEvent e) { dismissOverlay.show(); }
+					});
+			@Override public boolean onTouch(View v, MotionEvent evento) {
+				detector.onTouchEvent(evento);
+				return true;
+			}
+		});
+		misPuntos.setOnTouchListener(new View.OnTouchListener() {
+			GestureDetector detector = new DireccionesGestureDetector(
+					Contador.this,
+					new DireccionesGestureDetector
+							.SimpleOnDireccionesGestureListener() {
+						@Override public boolean onDerecha(MotionEvent e1,
+														   MotionEvent e2, float distX, float distY) {
+							partida.puntoPara(true);
+							vibrador.vibrate(vibrEntrada, -1);
+							actualizaNumeros();
+							return true;
+						}
+						@Override
+						public void onLongPress(MotionEvent e) { dismissOverlay.show(); }
+					});
+			@Override public boolean onTouch(View v, MotionEvent evento) {
+				detector.onTouchEvent(evento);
+				return true;
+			}
+		});
+		susPuntos.setOnTouchListener(new View.OnTouchListener() {
+			GestureDetector detector = new DireccionesGestureDetector(
+					Contador.this,
+					new DireccionesGestureDetector
+							.SimpleOnDireccionesGestureListener() {
+						@Override public boolean onDerecha(MotionEvent e1,
+														   MotionEvent e2, float distX, float distY) {
+							partida.puntoPara(false);
+							vibrador.vibrate(vibrEntrada, -1);
+							actualizaNumeros();
+							return true;
+						}
+						@Override
+						public void onLongPress(MotionEvent e) { dismissOverlay.show(); }
+					});
+			@Override public boolean onTouch(View v, MotionEvent evento) {
+				detector.onTouchEvent(evento);
+				return true;
+			}
+		});
+
+		dismissOverlay = (DismissOverlayView) findViewById(R.id.dismiss_overlay);
+		dismissOverlay.setIntroText(
+				"Para salir de la aplicación, haz una pulsación larga");
+		dismissOverlay.showIntroIfNecessary();
+	}
+	void actualizaNumeros() {
+		misPuntos.setText(partida.getMisPuntos());
+		susPuntos.setText(partida.getSusPuntos());
+		misJuegos.setText(partida.getMisJuegos());
+		susJuegos.setText(partida.getSusJuegos());
+		misSets.setText(partida.getMisSets());
+		susSets.setText(partida.getSusSets());
+	}
+	@Override public void onEnterAmbient(Bundle ambientDetails) {
+		super.onEnterAmbient(ambientDetails);
+		misPuntos.setTypeface(fuenteFina);
+		misPuntos.getPaint().setAntiAlias(false);
+		susPuntos.setTypeface(fuenteFina);
+		susPuntos.getPaint().setAntiAlias(false);
+		misJuegos.setTypeface(fuenteFina);
+		misJuegos.getPaint().setAntiAlias(false);
+		susJuegos.setTypeface(fuenteFina);
+		susJuegos.getPaint().setAntiAlias(false);
+		misSets.setTypeface(fuenteFina);
+		misSets.getPaint().setAntiAlias(false);
+		susSets.setTypeface(fuenteFina);
+		susSets.getPaint().setAntiAlias(false);
+		hora.setVisibility(View.VISIBLE);
+	}
+	@Override public void onExitAmbient() {
+		super.onExitAmbient();
+		misPuntos.setTypeface(fuenteNormal);
+		misPuntos.getPaint().setAntiAlias(false);
+		susPuntos.setTypeface(fuenteNormal);
+		susPuntos.getPaint().setAntiAlias(false);
+		misJuegos.setTypeface(fuenteNormal);
+		misJuegos.getPaint().setAntiAlias(false);
+		susJuegos.setTypeface(fuenteNormal);
+		susJuegos.getPaint().setAntiAlias(false);
+		misSets.setTypeface(fuenteNormal);
+		misSets.getPaint().setAntiAlias(false);
+		susSets.setTypeface(fuenteNormal);
+		susSets.getPaint().setAntiAlias(false);
+		hora.setVisibility(View.GONE);
+	}
+	@Override
+	public void onUpdateAmbient() {
+		super.onUpdateAmbient();
+		// Actualizar contenido
+		Calendar c = Calendar.getInstance();
+		c.setTime(new Date());
+		hora.setText(c.get(Calendar.HOUR_OF_DAY) + ":" + c.get(Calendar.MINUTE));
+	}
+}
